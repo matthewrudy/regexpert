a2fe7afa6b814827c9f8ad4c8cfe0982ca759d9a
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2008-12-07 17:42:58 +0000

    initial commit

---
+++ lib/regexpert.rb
@@ -0,0 +1,36 @@
+module Regexpert
+  module Format
+    # Taken from dm-more - http://github.com/sam/dm-more/tree/master/dm-validations/lib/dm-validations/formats/email.rb
+    # RFC2822 (No attribution reference available)
+    EmailAddress = begin
+      alpha = "a-zA-Z"
+      digit = "0-9"
+      atext = "[#{alpha}#{digit}\!\#\$\%\&\'\*+\/\=\?\^\_\`\{\|\}\~\-]"
+      dot_atom_text = "#{atext}+([.]#{atext}*)*"
+      dot_atom = "#{dot_atom_text}"
+      qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
+      text = "[\\x01-\\x09\\x11\\x12\\x14-\\x7f]"
+      quoted_pair = "(\\x5c#{text})"
+      qcontent = "(?:#{qtext}|#{quoted_pair})"
+      quoted_string = "[\"]#{qcontent}+[\"]"
+      atom = "#{atext}+"
+      word = "(?:#{atom}|#{quoted_string})"
+      obs_local_part = "#{word}([.]#{word})*"
+      local_part = "(?:#{dot_atom}|#{quoted_string}|#{obs_local_part})"
+      no_ws_ctl = "\\x01-\\x08\\x11\\x12\\x14-\\x1f\\x7f"
+      dtext = "[#{no_ws_ctl}\\x21-\\x5a\\x5e-\\x7e]"
+      dcontent = "(?:#{dtext}|#{quoted_pair})"
+      domain_literal = "\\[#{dcontent}+\\]"
+      obs_domain = "#{atom}([.]#{atom})*"
+      domain = "(?:#{dot_atom}|#{domain_literal}|#{obs_domain})"
+      addr_spec = "#{local_part}\@#{domain}"
+      pattern = /^#{addr_spec}$/
+    end
+
+    # Taken from dm-more http://github.com/sam/dm-more/tree/master/dm-validations/lib/dm-validations/formats/url.rb
+    Url = begin
+      # Regex from http://www.igvita.com/2006/09/07/validating-url-in-ruby-on-rails/
+      /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+    end
+  end
+end
