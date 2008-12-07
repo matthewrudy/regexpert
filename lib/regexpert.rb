47638f29c663f8f1206f9378df6b542237679196
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2008-12-07 18:06:55 +0000

    added UK postcode and some doctests

--- lib/regexpert.rb
+++ lib/regexpert.rb
@@ -1,7 +1,15 @@
 module Regexpert
   module Format
-    # Taken from dm-more - http://github.com/sam/dm-more/tree/master/dm-validations/lib/dm-validations/formats/email.rb
-    # RFC2822 (No attribution reference available)
+    # This is taken from dm-more - http://github.com/sam/dm-more/tree/master/dm-validations/lib/dm-validations/formats/email.rb
+    #     RFC2822 (No attribution reference available)
+    #
+    # doctest: email_address
+    #   >> "MatthewRudyJacobs@gmail.com" =~ Regexpert::Format::EmailAddress
+    #   => 0
+    #
+    #   >> "dev@" =~ Regexpert::Format::EmailAddress
+    #   => nil
+    #
     EmailAddress = begin
       alpha = "a-zA-Z"
       digit = "0-9"
@@ -27,10 +35,42 @@ module Regexpert
       pattern = /^#{addr_spec}$/
     end
 
-    # Taken from dm-more http://github.com/sam/dm-more/tree/master/dm-validations/lib/dm-validations/formats/url.rb
+    # This is taken from dm-more http://github.com/sam/dm-more/tree/master/dm-validations/lib/dm-validations/formats/url.rb
+    #     Regex from http://www.igvita.com/2006/09/07/validating-url-in-ruby-on-rails/
+    #
+    # doctest: url # examples from Rails auto_link tests
+    #   >> "http://www.rubyonrails.com/contact;new" =~ Regexpert::Format::Url
+    #   => 0
+    #   >> "http://maps.google.co.uk/maps?f=q&q=the+london+eye&ie=UTF8&ll=51.503373,-0.11939&spn=0.007052,0.012767&z=16&iwloc=A" =~ Regexpert::Format::Url
+    #   => 0
+    #   >> "http://en.wikipedia.org/wiki/Sprite_(computer_graphics)" =~ Regexpert::Format::Url
+    #   => 0
+    # TODO: think of a good example of a bad url
     Url = begin
-      # Regex from http://www.igvita.com/2006/09/07/validating-url-in-ruby-on-rails/
       /(^$)|(^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix
+    end
+    
+    # This is taken from Django.Contrib.Localflavor.uk
+    #     The regular expression used is sourced from the schema for British Standard
+    #     BS7666 address types: http://www.govtalk.gov.uk/gdsc/schemas/bs7666-v2-0.xsd
+    # 
+    # doctest: ukpostcode
+    #   >> "GIR   0AA" =~ Regexpert::Format::UKPostcode # GIR 0AA is a special GIRO postcode
+    #   => 0
+    #   >> "AL40XB" =~ Regexpert::Format::UKPostcode
+    #   => 0
+    #   >> "CB4 1TL" =~ Regexpert::Format::UKPostcode
+    #   => 0
+    #
+    #   >> "AL44 NOP" =~ Regexpert::Format::UKPostcode
+    #   => nil
+    #   >> "CB4-1TL" =~ Regexpert::Format::UKPostcode
+    #   => nil
+    #
+    UKPostcode = begin
+      outcode_pattern = '[A-PR-UWYZ]([0-9]{1,2}|([A-HIK-Y][0-9](|[0-9]|[ABEHMNPRVWXY]))|[0-9][A-HJKSTUW])'
+      incode_pattern = '[0-9][ABD-HJLNP-UW-Z]{2}'
+      postcode_regex = Regexp.new("^(GIR *0AA|#{outcode_pattern} *#{incode_pattern})$", Regexp::IGNORECASE)
     end
   end
 end
